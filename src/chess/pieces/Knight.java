63217611c23aaa3c95f45f87df8c8ff7b60ea998
Ethan Leung <ethan.leung12@gmail.com>
2024-01-08 01:22:54 -0500

    Created Skeleton function for board and move class. Defined first piece subclass 'Knight' and defined caclulateLegalMoves function logic. Created enum class Type to describe the piece's color (WHITE/BLACK).

---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,43 @@
+package chess.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import chess.Type;
+import chess.board.*;
+
+public class Knight extends Piece {
+
+    private final static int[] POSSIBLE_MOVES = {-17, -15, -10, 6, 6, 10, 15, 17};
+
+    private Knight(final int position, Type pieceType){
+        super(position, pieceType);
+    }
+
+    @Override
+    public List<Move> calculateLegalMoves(Board board) {
+        
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(int i = 0; i < POSSIBLE_MOVES.length; i++){
+            if(this.position + POSSIBLE_MOVES[i] >= 0 && this.position + POSSIBLE_MOVES[i] <= 63) {
+                
+                final Tile possibleDestination = board.getTile(this.position + POSSIBLE_MOVES[i]);
+
+                if(!possibleDestination.isTileOccupied()) { // If the tile is not occupied
+                    legalMoves.add(new Move());
+                } else {
+                    final Piece pieceAtDestination = possibleDestination.getPiece();
+                    final Type type = pieceAtDestination.getType();
+                    if(this.pieceType != type) legalMoves.add(new Move());
+                }
+
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+    
+}
--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -1,5 +1,26 @@
+/* Start of Package declarations */
 package chess.pieces;
+/* End of package declarations */
 
-public class Piece {
+/* Start of package imports */
+import java.util.List;
+import chess.Type;
+import chess.board.*;
+/* End of package imports*/
 
+/* The piece class to describe each type of piece in the Chess game.
+*
+*/
+public abstract class Piece {
+
+    protected final int position;
+    protected final Type pieceType;
+
+    private Piece(final int position, Type pieceType){
+        this.position = position;
+        this.pieceType = pieceType;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
+    
 }
